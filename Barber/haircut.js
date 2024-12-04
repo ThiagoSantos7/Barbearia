7e63947adbb042e7bc1acbc044621d81d3c2759e
ThiagoSantos7 <t.roberto.santos2024@gmail.com>
2024-12-04 17:50:41 -0300

    first commit

---
+++ Barber/haircut.js
@@ -0,0 +1,5 @@
+const haircut = [
+    { src: './assets/corteCabelo.jpg', name: "Corte de Cabelo", price: 45 },
+    { src: './assets/barbaCompleta.jpg', name: "Barba Completa", price: 35 },
+    { src: './assets/cabeloBarba.jpg', name: "Cabelo e barba", price: 70 },
+]
