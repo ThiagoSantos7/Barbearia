8b02a8c96168f512e201b24d2b39ac587da94473
ThiagoSantos7 <t.roberto.santos2024@gmail.com>
2024-12-28 17:51:39 -0300

    noavas alterações em javaScript

--- Barber/scripts.js
+++ Barber/scripts.js
@@ -1,6 +1,8 @@
 
 const buttonService = document.querySelector('.buttonServices')
+const buttonTwo = document.querySelector('.buttonServicesTwo')
 const list = document.querySelector('.listCortes')
+
 
 
 function formatCurrency(value) {
@@ -10,22 +12,31 @@ function formatCurrency(value) {
 }
 
 const hairCort = () => {
+
     let myLi = ''
 
-    haircut.forEach(element => {
-        myLi +=
-            `
-                <li class="liCut">
-                    <p class="pMain">${formatCurrency(element.name)}</p>
-                     <a href="https://api.whatsapp.com/send?phone=5511985214062&text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20corte!"><button class="buttonPrice">${formatCurrency(element.price)}</button></a>
-                </li>
+    if (list.style.display === "none" ) {
+
+        list.style.display = "grid"
+
+        haircut.forEach(element => {
+            myLi +=
                 `
-    });
+                    <li class="liCut">
+                        <p class="pMain">${formatCurrency(element.name)}</p>
+                         <a href="https://api.whatsapp.com/send?phone=5511985214062&text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20corte!"><button class="buttonPrice">${formatCurrency(element.price)}</button></a>
+                    </li>
+                    `
+        });
+    } else {
+
+        list.style.display = "none"
+    }
 
 
     list.innerHTML = myLi
 
-    
+
 }
 
 buttonService.addEventListener("click", hairCort)
