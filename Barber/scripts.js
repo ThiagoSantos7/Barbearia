d160ba02a9b79ca7b2bde04132bb4dba62eecbf8
ThiagoSantos7 <t.roberto.santos2024@gmail.com>
2024-12-29 17:53:31 -0300

    alterações em todos os arquivos

--- Barber/scripts.js
+++ Barber/scripts.js
@@ -2,7 +2,9 @@
 const buttonService = document.querySelector('.buttonServices')
 const buttonTwo = document.querySelector('.buttonServicesTwo')
 const list = document.querySelector('.listCortes')
-
+const buttonDomicilio = document.querySelector('.domicilio')
+const formulario = document.querySelector('.formulario')
+const mascara = document.querySelector('.mascara-formulario')
 
 
 function formatCurrency(value) {
@@ -15,28 +17,39 @@ const hairCort = () => {
 
     let myLi = ''
 
-    if (list.style.display === "none" ) {
+    haircut.forEach(element => {
+        myLi +=
+            `
+               <li class="liCut">
+                    <p class="pMain">${formatCurrency(element.name)}</p>
+                     <a href="https://api.whatsapp.com/send?phone=5511985214062&text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20corte!"><button class="buttonPrice">${formatCurrency(element.price)}</button></a>
+                </li>
+                 
+            `
 
-        list.style.display = "grid"
-
-        haircut.forEach(element => {
-            myLi +=
-                `
-                    <li class="liCut">
-                        <p class="pMain">${formatCurrency(element.name)}</p>
-                         <a href="https://api.whatsapp.com/send?phone=5511985214062&text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20corte!"><button class="buttonPrice">${formatCurrency(element.price)}</button></a>
-                    </li>
-                    `
-        });
-    } else {
-
-        list.style.display = "none"
-    }
-
+    });
 
     list.innerHTML = myLi
 
-
 }
 
-buttonService.addEventListener("click", hairCort)
+
+const aparecerForm = () => {
+    mascara.style.visibility = "visible"
+    formulario.style.left = "50%"
+    formulario.style.transform = "translateX(-50%)"
+}
+
+
+const esconderform = () => {
+    mascara.style.visibility = "hidden"
+    formulario.style.left = "-320px"
+    formulario.style.transform = "translateX(0)"
+}
+
+
+
+
+mascara.addEventListener("click", esconderform)
+buttonDomicilio.addEventListener("click", aparecerForm)
+buttonService.addEventListener("click", hairCort) 
