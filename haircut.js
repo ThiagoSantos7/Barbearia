886d063921375faa713fdf24589929035a9d6cd8
ThiagoSantos7 <t.roberto.santos2021@gmail.com>
2025-01-27 13:59:50 -0300

    alterações de detalhes

--- haircut.js
+++ haircut.js
@@ -3,13 +3,8 @@ const haircut = [
     { name: "Cortes degradê penteado", price: 50, src: "https://api.whatsapp.com/send?phone=5511985214062&text=Ol%C3%A1,%20quero%20agendar%20um%20Corte%20degrad%C3%AA%20com%20Penteado%20-%20R$%2050,00"},
     { name: "Sobrancelha", price: 15, src: "https://api.whatsapp.com/send?phone=5511985214062&text=Ol%C3%A1,%20quero%20fazer%20a%20Sobrancelha%20-%20R$%2015,00" },
     { name: "Perfil (Pézinho)", price: 15, src: "https://api.whatsapp.com/send?phone=5511985214062&text=Ol%C3%A1,%20quero%20Perfil%20(P%C3%A9zinho)%20-%20R$%2015,00" },
-<<<<<<< HEAD
     { name: "Relaxamento a partir de", price: 40, src: "https://api.whatsapp.com/send?phone=5511985214062&text=Ol%C3%A1,%20quero%20fazer%20um%20relaxamento%20-%20R$%2040,00" },
-    { name: "Luzes", price: 60, src: "https://wa.me/5511985214062?text=Ol%C3%A1,%20quero%20fazer%20Luzes%20-%20R$%2060,00" },
-=======
-    { name: "Relaxamento", price: 40, src: "https://api.whatsapp.com/send?phone=5511985214062&text=Ol%C3%A1,%20quero%20fazer%20um%20relaxamento%20-%20R$%2040,00" },
     { name: "Luzes a partir de", price: 60, src: "https://wa.me/5511985214062?text=Ol%C3%A1,%20quero%20fazer%20Luzes%20-%20R$%2060,00" },
->>>>>>> 5ec024b26419bbf61cd0922c68b7589fd055d1d7
     { name: "Progressiva", price: 70, src: "https://wa.me/5511985214062?text=Ol%C3%A1,%20quero%20fazer%20uma%20Progressiva%20-%20R$%2070,00" },
     { name: "Barba Completa", price: 30, src: "https://wa.me/5511985214062?text=Ol%C3%A1,%20quero%20Barba%20Completa%20-%20R$%2030,00" },
     { name: "Cabelo e barba", price: 70, src: "https://wa.me/5511985214062?text=Ol%C3%A1,%20quero%20Cabelo%20e%20Barba%20-%20R$%2070,00" },
